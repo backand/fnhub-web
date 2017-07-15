40a589e3d2efc81d3950469eb36e00417b3f1df4
Mohan Singh <singhmohancs@gmaIl.com>
2017-07-16 00:57:33 +0530

    Removed angular version

--- src/client/app/+auth/reset-password/reset-password.component.ts
+++
@@ -1,9 +0,0 @@
-// angular
-import { Component } from '@angular/core';
-
-@Component({
-  templateUrl: './reset-password.component.html',
-  styleUrls: ['./reset-password.component.scss']
-})
-export class ResetPasswordComponent {
-}
--- src/client/app/+auth/signin/signin.component.ts
+++
@@ -1,9 +0,0 @@
-// angular
-import { Component } from '@angular/core';
-
-@Component({
-  templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.scss']
-})
-export class SignInComponent {
-}
