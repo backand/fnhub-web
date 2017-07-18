827878318c05e83f261588af29b9a8fb35f4cba6
Mohan Singh <singhmohancs@gmaIl.com>
2017-07-18 15:28:36 +0530

    Removed System config

--- client/src/app/about/about.component.ts
+++ client/src/app/about/about.component.ts
@@ -46,23 +46,7 @@ export class AboutComponent implements OnInit {
      * console.log('mockData', mockData);
      * if you're working with mock data you can also use http.get('assets/mock-data/mock-data.json')
      */
-    this.asyncDataWithWebpack();
   }
-  private asyncDataWithWebpack() {
-    /**
-     * you can also async load mock data with 'es6-promise-loader'
-     * you would do this if you don't want the mock-data bundled
-     * remember that 'es6-promise-loader' is a promise
-     */
-    setTimeout(() => {
-
-      System.import('../../assets/mock-data/mock-data.json')
-        .then((json) => {
-          console.log('async mockData', json);
-          this.localState = json;
-        });
-
-    });
-  }
+ 
 
 }
